4a020b3304467dcdfa7c92e7bc2afe35e3e27a73
luminousky <yeonwookkim1296@gmail.com>
2026-05-22 20:36:49 +0900

    feat: ProjectRepository 및 UserRepository 작성

---
+++ core/src/main/java/org/issuetracker/repository/UserRepository.java
@@ -0,0 +1,43 @@
+package org.issuetracker.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.issuetracker.model.User;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    private final File file = new File("users.json");
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public List<User> findAll() {
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try {
+            return mapper.readValue(
+                    file,
+                    mapper.getTypeFactory()
+                            .constructCollectionType(
+                                    List.class,
+                                    User.class
+                            )
+            );
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public void saveAll(List<User> users) {
+        try {
+            mapper.writerWithDefaultPrettyPrinter()
+                    .writeValue(file, users);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
